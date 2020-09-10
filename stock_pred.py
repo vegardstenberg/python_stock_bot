9aaff132db44972ff69e7c607eaa6a26409fd00a
Vegard Hansen Stenberg <vegard.koder@gmail.com>
2020-09-10 14:58:21 +0200

    Have made it functional I think for 1 stock, will test later

--- stock_pred.py
+++ stock_pred.py
@@ -8,16 +8,17 @@ Created on Tue Aug 25 13:30:53 2020
 import stock_info as si
 import time
 
-stock = 0
+stock_name = ""
 
-for gainers in si.get_gainers(5):
+for gainers in si.get_gainers(25):
     try:
         stock = si.get_stock_data(gainers)
+        stock_name = gainers
         break;
     except:
         print("Could not invest in:", gainers)
 
-if(stock == 0):
+if(stock.empty):
     print("Found no stocks at this time... shutting down...")
     exit()
 
@@ -33,17 +34,19 @@ def SMA(days):
     return s
 
 if(SMA(200) < SMA(50)):
-    print("invest!")
-    
+    print("invest!", stock_name, stock[0])
     buying_price = stock[0]
-    
     while True:
+        stock = si.get_stock_data(stock_name)
         price = stock[0]
         if ((price - buying_price) / buying_price) * 100 > 2:
             print("Sold at a profit of", price - buying_price)
             buying_price = 0
+            break;
         elif(SMA(200) > SMA(50) and buying_price != 0):
             print("Sold at a loss of", buying_price - stock[0])
+            buying_price = 0
+            break;
         time.sleep(600)
     
 print("SMA 200:", SMA(200))
